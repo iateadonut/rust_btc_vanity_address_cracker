723986455ba89d33ceffb674dc6979bec00072f2
Danny Boy <orienta.dan@gmail.com>
2023-03-30 17:56:12 +0900

    give estimated time to find a substring

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,10 @@ use std::fs::{File, OpenOptions};
 use std::io::prelude::*;
 use std::io::{self, BufRead};
 use std::path::Path;
+use std::time::Instant;
+
+const BASE58_CHARS: &str = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+const PRACTICE_RUNS: u32 = 10;
 
 fn create_keypair() -> (PrivateKey, Address) {
     let secp = secp256k1::Secp256k1::new();
@@ -44,6 +48,10 @@ fn read_target_substrings(file_path: &str) -> Result<Vec<String>, io::Error> {
     Ok(substrings)
 }
 
+fn is_base58(s: &str) -> bool {
+    s.chars().all(|c| BASE58_CHARS.contains(c))
+}
+
 fn main() {
     let max_threads = 2;
     let output_file = "keypairs.txt";
@@ -60,7 +68,65 @@ fn main() {
         }
     };
 
+    for s in &target_substrings {
+        if !is_base58(s) {
+            eprintln!("Error: '{}' contains non-Base58 characters.", s);
+            return;
+        }
+    }
+
     let (tx, rx) = mpsc::channel();
+
+    // Run practice rounds to measure the performance
+    let mut durations = Vec::new();
+    for _ in 0..PRACTICE_RUNS {
+        let start_time = Instant::now();
+        let (_private_key, _address) = create_keypair();
+        durations.push(start_time.elapsed().as_micros());
+    }
+
+    // Calculate the median duration
+    durations.sort();
+    let median_duration = durations[durations.len() / 2];
+
+    // Calculate the probability of finding a matching address
+
+    let address_length = 34;
+    let base58_length = 58;
+
+    // Find the longest substring in the list
+    let longest_substring = target_substrings.iter().max_by_key(|s| s.len()).unwrap();
+    let longest_substring_length = longest_substring.len();
+
+    if longest_substring_length == address_length {
+        println!("Don't be daft. Trying to crack a BTC address is basically impossible and trying is a terrible waste of resources.");
+        return;
+    }
+
+    // Find the shortest substring in the list
+    let shortest_substring = target_substrings.iter().min_by_key(|s| s.len()).unwrap();
+    let target_length = shortest_substring.len();
+
+    let probability = 1.0 - (1.0 - (1.0 / base58_length as f64).powi(target_length as i32)).powi((address_length - target_length + 1) as i32);
+    let attempts_needed = (1.0 / probability).ceil() as u64;
+    let estimated_time = (attempts_needed as u128 * median_duration) / 1_000_000;
+
+    println!(
+        "Estimated time to find a matching address: {} seconds",
+        estimated_time
+    );
+
+    println!("Are you sure you want to proceed? (y/n)");
+
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+
+    if input.trim().to_lowercase() != "y" {
+        return;
+    }
+
+    let search_start_time = Instant::now();
+
     let found = Arc::new(AtomicBool::new(false));
     let finished_threads = Arc::new(AtomicUsize::new(0));
 
@@ -106,8 +172,12 @@ fn main() {
 
     match rx.try_recv() {
         Ok((private_key, address)) => {
+
+            let elapsed_time = search_start_time.elapsed().as_secs();
+
             println!("Found private key: {}", private_key.to_wif());
             println!("Found address: {}", address);
+            println!("Time taken to find the matching address: {} seconds", elapsed_time);
 
             let mut file = OpenOptions::new()
                 .write(true)
